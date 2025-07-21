7eec831185da52a454a835c9b5a4bdc9de098a49
Sergey Kucherenko <skv026@bk.ru>
2025-07-21 17:05:13 +0300

    added model Run

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Run(models.Model):
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True)
